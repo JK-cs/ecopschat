d8ad05dfba181f16cb3a6b840002d3af31f7312d
JK-cs <wskje0106@ewhain.net>
2023-02-23 07:29:10 +0900

    setdefault

--- ecopschat/settings.py
+++ ecopschat/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 from environ import Env
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,11 +51,11 @@ if "CHANNEL_LAYER_REDIS_URL" in env:
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.get('DJANGO_SECRET_KEY', 'django-insecure-ccpa_dgd-*m0a)#nd#u&bhrt&$#i5&u*eu&l4$au9+z-748(&0')
+SECRET_KEY = os.environ.setdefault('DJANGO_SECRET_KEY', 'django-insecure-ccpa_dgd-*m0a)#nd#u&bhrt&$#i5&u*eu&l4$au9+z-748(&0')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(env.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.setdefault('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
